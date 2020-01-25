87bd288f30c5b5318c36d711ea33ae3f37ac8697
Garrett Thornburg <film42@gmail.com>
2020-01-25 11:53:35 -0500

    Ok, things seem to work without unwrap

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,14 @@ extern crate failure;
 mod pb;
 mod rpc;
 
-// use futures::stream::StreamExt;
-// use pb::rpc::Request;
-use pb::warehouse::{ShipmentRequest, Shipments};
+use rpc::GetResponseProtoable;
+
+use pb::rpc::Response as RpcResponse;
+use pb::warehouse::{Shipment, ShipmentRequest, Shipments};
 use protobuf::Message;
 use rants::Client;
-// use std::time::Duration;
-// use tokio::runtime::Runtime;
-// use tokio::time::timeout;
+use tower::layer::Layer;
 use tower::Service;
-// use uuid::Uuid;
 
 use std::future::Future;
 use std::pin::Pin;
@@ -29,22 +27,69 @@ pub struct ShipmentsRpcClient {
     nats: Client,
 }
 
-impl rpc::Requestable for ShipmentRequest {
-    fn service_name() -> String {
-        "Warehouse::ShipmentService".to_string()
-    }
-    fn method_name() -> String {
-        "search".to_string()
-    }
-}
-
 impl ShipmentsRpcClient {
     fn new(nats: Client) -> ShipmentsRpcClient {
         ShipmentsRpcClient { nats: nats }
     }
 }
 
-impl Service<ShipmentRequest> for ShipmentsRpcClient {
+pub enum ShipmentEndpoint {
+    Search(ShipmentRequest),
+    Update(Shipment),
+    Create(Shipment),
+    NotImplemented(Shipment),
+}
+
+// impl rpc::Requestable for ShipmentRequest {
+//     fn service_name(&self) -> String {
+//         "Warehouse::ShipmentService".to_string()
+//     }
+//     fn method_name(&self) -> String {
+//         "search".to_string()
+//     }
+//     fn into_bytes(&self) -> Result<Vec<u8>, failure::Error> {
+//         let bytes = self.write_to_bytes()?;
+//         Ok(bytes)
+//     }
+// }
+
+// impl rpc::Requestable for Shipment {
+//     fn service_name(&self) -> String {
+//         "Warehouse::ShipmentService".to_string()
+//     }
+//     fn method_name(&self) -> String {
+//         "create".to_string()
+//     }
+//     fn into_bytes(&self) -> Result<Vec<u8>, failure::Error> {
+//         let bytes = self.write_to_bytes()?;
+//         Ok(bytes)
+//     }
+// }
+
+impl rpc::Requestable for ShipmentEndpoint {
+    fn service_name(&self) -> String {
+        "Warehouse::ShipmentService".to_string()
+    }
+    fn method_name(&self) -> String {
+        match self {
+            ShipmentEndpoint::Search(_) => "search".to_string(),
+            ShipmentEndpoint::Update(_) => "update".to_string(),
+            ShipmentEndpoint::Create(_) => "create".to_string(),
+            ShipmentEndpoint::NotImplemented(_) => "not_implemented".to_string(),
+        }
+    }
+    fn into_bytes(&self) -> Result<Vec<u8>, failure::Error> {
+        let bytes = match self {
+            ShipmentEndpoint::Search(pb) => pb.write_to_bytes()?,
+            ShipmentEndpoint::Update(pb) => pb.write_to_bytes()?,
+            ShipmentEndpoint::Create(pb) => pb.write_to_bytes()?,
+            ShipmentEndpoint::NotImplemented(pb) => pb.write_to_bytes()?,
+        };
+        Ok(bytes)
+    }
+}
+
+impl Service<ShipmentEndpoint> for ShipmentsRpcClient {
     type Response = Shipments;
     type Error = failure::Error;
     type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
@@ -53,18 +98,64 @@ impl Service<ShipmentRequest> for ShipmentsRpcClient {
         Poll::Ready(Ok(()))
     }
 
-    fn call(&mut self, req: ShipmentRequest) -> Self::Future {
+    fn call(&mut self, req: ShipmentEndpoint) -> Self::Future {
         let nats = self.nats.clone();
         Box::pin(async move {
-            let res = rpc::call(&nats, req).await?;
+            //let res = rpc::call(&nats, req).await?;
             let mut shipments = Shipments::new();
-            shipments.merge_from_bytes(res.get_response_proto())?;
+            //shipments.merge_from_bytes(res.get_response_proto())?;
             Ok(shipments)
         })
     }
 }
 
-use tower::{ServiceBuilder, ServiceExt};
+use pb::rpc::Request as RpcRequest;
+
+use tower::util::BoxService;
+
+pub struct ShipmentClient<S> {
+    rpc_client: S,
+}
+
+impl<S> ShipmentClient<S>
+where
+    S: Service<RpcRequest> + Clone,
+    <S as Service<RpcRequest>>::Response: GetResponseProtoable,
+    <S as Service<RpcRequest>>::Error: std::fmt::Debug,
+    //<S as Service<RpcRequest>>::Error: Into<Box<dyn std::error::Error + Send + Sync + 'static>>,
+{
+    async fn do_request<Req, Res>(&self, req: Req) -> Result<Res, failure::Error>
+    where
+        Req: rpc::Requestable,
+        Res: protobuf::Message,
+    {
+        let mut rpc_client = self.rpc_client.clone();
+        let req: RpcRequest = rpc::build_request(req)?;
+        let res = rpc_client
+            .call(req)
+            .await
+            .map_err(|e| format_err!("Error: {:?}", e))?;
+        let mut res_pb = Res::new();
+        res_pb.merge_from_bytes(res.get_response_proto())?;
+        Ok(res_pb)
+    }
+
+    pub async fn search(&self, req: ShipmentRequest) -> Result<Shipments, failure::Error> {
+        self.do_request(ShipmentEndpoint::Search(req)).await
+    }
+
+    pub async fn create(&self, req: Shipment) -> Result<Shipment, failure::Error> {
+        self.do_request(ShipmentEndpoint::Create(req)).await
+    }
+
+    pub async fn not_implemented(&self, req: Shipment) -> Result<Shipment, failure::Error> {
+        self.do_request(ShipmentEndpoint::NotImplemented(req)).await
+    }
+
+    pub async fn update(&self, req: Shipment) -> Result<Shipment, failure::Error> {
+        self.do_request(ShipmentEndpoint::Update(req)).await
+    }
+}
 
 #[tokio::main]
 async fn main() {
@@ -73,42 +164,90 @@ async fn main() {
     // Connect to the server
     client.connect().await;
 
-    let rpc_client = ServiceBuilder::new()
-        .buffer(100)
-        .concurrency_limit(100)
-        .service(ShipmentsRpcClient::new(client.clone()));
+    {
+        use tower::{ServiceBuilder, ServiceExt};
+        let rpc_client_srv = rpc::RpcClient::new(client.clone());
+        let mut rpc_client = ServiceBuilder::new()
+            .buffer(100)
+            .concurrency_limit(100)
+            .service(rpc_client_srv.clone());
 
-    let futures: Vec<_> = (1..100)
-        .into_iter()
-        .map(|i| {
-            let mut rpc_client = rpc_client.clone();
-            tokio::spawn(async move {
-                let mut req = ShipmentRequest::new();
-                let mut args = protobuf::RepeatedField::new();
-                args.push(i.to_string());
-                req.set_guid(args);
-                match rpc_client.ready().await {
-                    Ok(_) => {
-                        println!("Got response for: {}", i);
-                        match rpc_client.call(req).await {
-                            Ok(shipments) => {
-                                //let shipments = search_shipments(&client).await.unwrap();
-                                for shipment in shipments.get_records().iter() {
-                                    println!("Shipment guid: {}", shipment.get_guid());
-                                }
-                            }
-                            Err(err) => println!("Error: {}", err),
-                        }
-                    }
-                    Err(err) => println!("Error: {}", err),
-                };
-            })
-        })
-        .collect();
+        println!("Started!");
+        let sc = ShipmentClient {
+            rpc_client: rpc_client,
+        };
+        let req = ShipmentRequest::new();
+        //let mut res: Shipments = sc.do_request(ShipmentEndpoint::Search(req)).await.unwrap();
+        let mut res: Shipments = sc.search(req).await.unwrap();
+        let mut shipment: Shipment = res.take_records().pop().unwrap();
+        println!("First: {:?}", shipment);
+        //  shipment.set_address("1337 Apple Ln".to_string());
+        //  let res: Shipment = sc.call(ShipmentEndpoint::Create(shipment)).await.unwrap();
+        //  println!("Second: {:?}", res);
+    }
+
+    // {
+    //     let sc = ShipmentClient{nats: client.clone()};
+    //     let shipments = sc.search(ShipmentRequest::new()).await.unwrap();
+    //     println!("Shipments: {:?}", shipments);
+    // }
+
+    // {
+    //     use tower::{ServiceBuilder, ServiceExt};
+    //     let mut rpc_client = ServiceBuilder::new()
+    //         .buffer(100)
+    //         .concurrency_limit(100)
+    //         .service(ShipmentsRpcClient::new(client.clone()));
+
+    //     let mut req = ShipmentRequest::new();
+    //     let mut args = protobuf::RepeatedField::new();
+    //     args.push("1337".to_string());
+    //     req.set_guid(args);
+    //     rpc_client.ready().await.unwrap();
+    //     let mut res: Shipments = rpc_client
+    //         .call(ShipmentEndpoint::Search(req))
+    //         .await
+    //         .unwrap();
+
+    //     let mut shipment: Shipment = res.take_records().pop().unwrap();
+    //     println!("First: {:?}", shipment);
+    //     shipment.set_address("1337 Apple Ln".to_string());
+    //     let res = rpc_client.call(ShipmentEndpoint::Create(shipment)).await;
+    //     //.unwrap();
+    //     println!("Second: {:?}", res);
+    // }
+
+    // let futures: Vec<_> = (1..100)
+    //     .into_iter()
+    //     .map(|i| {
+    //         let mut rpc_client = rpc_client.clone();
+    //         tokio::spawn(async move {
+    //             let mut req = ShipmentRequest::new();
+    //             let mut args = protobuf::RepeatedField::new();
+    //             args.push(i.to_string());
+    //             req.set_guid(args);
+    //             match rpc_client.ready().await {
+    //                 Ok(_) => {
+    //                     println!("Got response for: {}", i);
+    //                     match rpc_client.call(ShipmentEndpoint::Search(req)).await {
+    //                         Ok(shipments) => {
+    //                             //let shipments = search_shipments(&client).await.unwrap();
+    //                             for shipment in shipments.get_records().iter() {
+    //                                 println!("Shipment guid: {}", shipment.get_guid());
+    //                             }
+    //                         }
+    //                         Err(err) => println!("Error: {}", err),
+    //                     }
+    //                 }
+    //                 Err(err) => println!("Error: {}", err),
+    //             };
+    //         })
+    //     })
+    //     .collect();
 
     // futures.iter().map(|f| f.await);
 
-    futures::future::join_all(futures).await;
+    // futures::future::join_all(futures).await;
 
     // for i in 1..100 {
     //     let mut rpc_client = rpc_client.clone();
--- src/rpc.rs
+++ src/rpc.rs
@@ -6,40 +6,44 @@ use rants::{Client, Subject};
 use std::time::Duration;
 //use tokio::runtime::Runtime;
 use tokio::time::timeout;
-// use tower::Service;
+use tower::Service;
 use uuid::Uuid;
 
-// use std::future::Future;
-// use std::pin::Pin;
-// use std::task::{Context, Poll};
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
 
 const ACK_TIMEOUT: Duration = Duration::from_secs(5);
 const RESPONSE_TIMEOUT: Duration = Duration::from_secs(60);
 const ACK_MSG: u8 = '\u{0001}' as u8;
 const NACK_MSG: u8 = '\u{0002}' as u8;
 
-fn new_inbox() -> Subject {
-    Uuid::new_v4().to_string().parse().unwrap()
+fn new_inbox() -> Result<Subject, failure::Error> {
+    // TODO: I'm struggling to grasp why this needs the Ok(_?).
+    Ok(Uuid::new_v4().to_string().parse()?)
 }
 
 pub async fn send_request_through_nats(
     nats: &Client,
-    subject: String,
     req: &RpcRequest,
 ) -> Result<RpcResponse, failure::Error> {
-    let subject = subject.parse()?;
-    let inbox = new_inbox();
+    let subject = build_subject_from_request(&req).parse()?;
+    let inbox = new_inbox()?;
     let (sid, mut sub) = nats.subscribe(&inbox, 2).await?;
     nats.unsubscribe_with_max_msgs(sid, 2).await?;
 
-    let payload = req.write_to_bytes().unwrap();
+    let payload = req.write_to_bytes()?;
     nats.publish_with_reply(&subject, &inbox, &payload).await?;
 
-    let msg1 = timeout(ACK_TIMEOUT, sub.next()).await?.unwrap();
+    let msg1 = timeout(ACK_TIMEOUT, sub.next())
+        .await?
+        .expect("None should never be returned here");
     if msg1.payload() == [NACK_MSG] {
         bail!("Retry later! Server sent NACK");
     }
-    let msg2 = timeout(RESPONSE_TIMEOUT, sub.next()).await?.unwrap();
+    let msg2 = timeout(RESPONSE_TIMEOUT, sub.next())
+        .await?
+        .expect("None should never be returned here");
 
     let res = match (msg1.payload(), msg2.payload()) {
         ([ACK_MSG], bytes) | (bytes, [ACK_MSG]) => {
@@ -84,20 +88,64 @@ pub fn build_subject_from_request(req: &RpcRequest) -> String {
 }
 
 pub trait Requestable {
-    fn service_name() -> String;
-    fn method_name() -> String;
+    fn service_name(&self) -> String;
+    fn method_name(&self) -> String;
+    fn into_bytes(&self) -> Result<Vec<u8>, failure::Error>;
 }
 
-pub async fn call<R>(nats: &Client, req: R) -> Result<RpcResponse, failure::Error>
+pub fn build_request<R>(req: R) -> Result<RpcRequest, failure::Error>
 where
-    R: Requestable + ::protobuf::Message,
+    R: Requestable,
 {
-    let req_bytes = req.write_to_bytes()?;
+    let req_bytes = req.into_bytes()?;
     let mut pb_req = RpcRequest::new();
     pb_req.set_request_proto(req_bytes);
-    pb_req.set_service_name(R::service_name());
-    pb_req.set_method_name(R::method_name());
+    pb_req.set_service_name(req.service_name());
+    pb_req.set_method_name(req.method_name());
     pb_req.set_caller("pb-nats-rs".to_string());
-    let sub = build_subject_from_request(&pb_req);
-    send_request_through_nats(&nats, sub, &pb_req).await
+    Ok(pb_req)
+}
+
+// pub async fn call<R>(nats: &Client, req: R) -> Result<RpcResponse, failure::Error>
+// where
+//     R: Requestable,
+// {
+//     let pb_req: RpcRequest = build_request(req)?;
+//     send_request_through_nats(&nats, &pb_req).await
+// }
+
+#[derive(Clone)]
+pub struct RpcClient {
+    nats: Client,
+}
+
+impl RpcClient {
+    pub fn new(nats_client: Client) -> RpcClient {
+        RpcClient { nats: nats_client }
+    }
+}
+
+pub trait GetResponseProtoable {
+    fn get_response_proto(&self) -> &[u8];
+}
+
+impl GetResponseProtoable for RpcResponse {
+    fn get_response_proto(&self) -> &[u8] {
+        self.get_response_proto()
+    }
+}
+
+impl Service<RpcRequest> for RpcClient {
+    type Response = RpcResponse;
+    type Error = failure::Error;
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, req: RpcRequest) -> Self::Future {
+        let nats = self.nats.clone();
+        Box::pin(async move { send_request_through_nats(&nats, &req).await })
+    }
 }
